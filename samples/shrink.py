ee80251716628e9b0e013539599d5dc7bd0cfad7
Ivan Podkidyshev <raashicat@gmail.com>
2019-02-21 11:57:23 +0400

    completed samples - 3 rus, 3 eng, everything in UTF-8

---
+++ samples/shrink.py
@@ -0,0 +1,29 @@
+# usage: %user%> samples/shrink.py samples
+from os import listdir
+from os.path import join, isdir
+from sys import argv
+
+
+def process(file):
+    print('PROCESSING: {}'.format(file))
+    with open(file, encoding='utf-8') as f:
+        lines = f.readlines()
+    with open(file, 'w', encoding='utf-8') as f:
+        f.writelines(filter(lambda el: len(el.strip()), lines))
+
+
+def scan(directory):
+    for entry in listdir(directory):
+        path = join(directory, entry)
+        if entry[-3:] == 'txt':
+            process(path)
+        elif isdir(path):
+            scan(path)
+
+
+def main():
+    scan(argv[1])
+
+
+if __name__ == '__main__':
+    main()
